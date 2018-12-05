b5da74c1b91dcf690945d3e59c81ca7165a44326
misterwhybe <wiebejelsma@hotmail.com>
2018-12-05 22:56:09 +0100

    scatterplot

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -3,8 +3,6 @@
 var womenInScience = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
 var consConf = "http://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015"
 
-
-
 window.onload = function() {
     var requests = [d3.json(womenInScience), d3.json(consConf)];
     FRA_con = []
@@ -13,6 +11,7 @@ window.onload = function() {
     NLD_con = []
     PRT_con = []
     GBR_con = []
+
     FRA_years = []
     DEU_years = []
     KOR_years = []
@@ -20,145 +19,187 @@ window.onload = function() {
     PRT_years = []
     GBR_years = []
 
+    women = []
+    women_years = []
+    consumers = []
 
     Promise.all(requests).then(function(response) {
         womenInScience = transformResponse(response[0])
         consConf = transformResponse(response[1])
-        console.log(womenInScience[0])
-        console.log(consConf)
+        missing =  [10, 12, 14, 16, 28, 30, 46]
+        console.log(consConf[1].Country)
         
+
+        for(i=0; i<47; i++){
+            women.push(womenInScience[i].datapoint)
+            women_years.push(womenInScience[i].time)
+
+        }
+        for(i=0; i<missing.length; i++){
+            women.splice(missing[i], 0, "None")
+        }
+        for(i=0; i<54; i++){
+            consumers.push(consConf[i].datapoint)
+        }
+        console.log(womenInScience)
+
+        // for(i = 0; i < 9; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+        //     FRA_con.push(consConf[i].datapoint)
+        //     FRA_years.push(consConf[i].time)
+        // }
+        // for(i = 9; i < 18; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+
+        //     NLD_con.push(consConf[i].datapoint)
+        //     NLD_years.push(consConf[i].time)
+        // }
+        // for(i = 18; i < 27; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+
+        //     PRT_con.push(consConf[i].datapoint)
+        //     PRT_years.push(consConf[i].time)
+        // }
+        // for(i = 27; i < 36; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+
+        //     DEU_con.push(consConf[i].datapoint)
+        //     DEU_years.push(consConf[i].time)
+        // }
+        // for(i = 36; i < 45; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+
+        //     GBR_con.push(consConf[i].datapoint)
+        //     GBR_years.push(consConf[i].time)
+        // }
+        // for(i = 45; i < 54; i +=1){
+        //     consumers.push(consConf[i].datapoint)
+
+        //     KOR_con.push(consConf[i].datapoint)
+        //     KOR_years.push(consConf[i].time)
+        // }
+
+        Dataset = []
+        Temp = []
+        for(i=0; i<54; i++){
+            Temp = [consumers[i], women[i]]
+            Dataset.push(Temp)
+        }
+        Data=[]
+        for(i=0; i<54; i++){
+            if(Dataset[i][1] !== "None"){
+                Data.push(Dataset[i])
+            }
+        }
+        console.log(Data)
+  
         
-        for(i = 0; i < 9; i +=1){
-            console.log(consConf[i].Country)
-
-            FRA_con.push(consConf[i].datapoint)
-            FRA_years.push(consConf[i].time)
-        }
-        for(i = 9; i < 18; i +=1){
-            console.log(consConf[i].Country)
-
-            NLD_con.push(consConf[i].datapoint)
-            NLD_years.push(consConf[i].time)
-        }
-        for(i = 18; i < 27; i +=1){
-            console.log(consConf[i].Country)
-
-            PRT_con.push(consConf[i].datapoint)
-            PRT_years.push(consConf[i].time)
-        }
-        for(i = 27; i < 36; i +=1){
-            console.log(consConf[i].Country)
-
-            DEU_con.push(consConf[i].datapoint)
-            DEU_years.push(consConf[i].time)
-        }
-        for(i = 36; i < 45; i +=1){
-            console.log(consConf[i].Country)
-            GBR_con.push(consConf[i].datapoint)
-            GBR_years.push(consConf[i].time)
-        }
-        for(i = 45; i < 54; i +=1){
-            console.log(consConf[i].Country)
-            KOR_con.push(consConf[i].datapoint)
-            KOR_years.push(consConf[i].time)
-        }
-
-            
-        // console.log(FRA_con)
-        // console.log(FRA_years)
-        // console.log(KOR_con)
-        // console.log(DEU_con)
-        // console.log(NLD_con)
-        // console.log(GBR_con)
-        // console.log(PRT_con)
-
-        // console.log(response[0].dataSets[0].series)
-        
-        
-        // console.log(response[0].dataSets[0].series["0:0"].observations)
-        // console.log(response[0].dataSets[0].series["0:1"].observations)
-        // console.log(response[0].dataSets[0].series["0:2"].observations)
-        // console.log(response[0].dataSets[0].series["0:3"].observations)
-        // console.log(response[0].dataSets[0].series["0:4"].observations)
-        // console.log(response[0].dataSets[0].series["0:5"].observations)
-        
-        // console.log(transformResponse(response[1]));
         d3.select("body")
-            .append("p").text("Renewable energy of Australia")
+            .append("p").text("Scatterplot")
             .append("p").text("By: Wiebe Jelsma (12468223)")
-            .append("p").text("ADD LINK");           
+            .append("p").text("The data of women in science can be found on:")
+            .append("p").text("http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/\
+            TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015")
+            .append("p").text("The data of consumer Confidence can be found on:")
+            .append("p").text(" http://stats.oecd.org/SDMX-JSON/data/HH_DASH/\
+            FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015");       
 
-            margin = {top: 5, right: 10, bottom: 40, left: 60}
+            margin = {top: 10, right: 25, bottom: 30, left: 30}
             var height = 400 - margin.top - margin.bottom;
             var width = 750 - margin.left - margin.right;
-            var barWidth = 15;
+            var padding = 40;
+            Colours1 = { "FRA":"red", "DEU":"black", "KOR":"gray", "NLD":"orange", "PRT":"green", "GBR":"blue"}
+            
 
             var xScale = d3.scaleLinear()
-                // .domain([1960, d3.max(Dates, function (d) {
-                //     return d;})])
-                // .range([margin.left, width - margin.left - margin.right]);
-                .domain([0,200])
-                .range([0,200])
+                    .domain([0, d3.max(Data, function(d) { return d[1]; })])
+                    .range([padding, width - padding * 2]);
             var yScale = d3.scaleLinear()
-                // .domain([0, d3.max(Values, function (d) {
-                //     return d;})])
-                // .range([height - margin.bottom ,margin.bottom]);
-                .domain([0,200])
-                .range([0,200])
+                    .domain([94, d3.max(Data, function(d) { return d[0]; })])
+                    .range([height - padding, padding]);
 
             var xAxis = d3.axisBottom(xScale)                                          
             var yAxis = d3.axisLeft(yScale);
 
             var svg = d3.select("body")
                 .append("svg")
-                .attr("width", width + margin.left)
-                .attr("height", height + margin.bottom)
-                .style("background", "lightyellow")
-                .append("g")
+                .attr("width", width)
+                .attr("height", height)
+                .style("background", "white")
+
             svg.selectAll("circle")
-                .data(NLD_con)
+                .data(Data)
                 .enter()
                 .append("circle")
-                .style("fill", "lightgreen")
-                .attr("width", barWidth)
-                .attr("height", function(d){
-                    return yScale(d);
+                .style("fill", function(d, i){
+                    if(i<9){
+                        // return Object.values(Colours1)[0]
+                        return "red"
+                    }
+                    if(i > 8 & i< 14){
+                        return "black"
+                    }
+                    if(i > 13 & i< 23){
+                        return "grey"
+                    }
+                    if(i > 22 &i< 30){
+                        return "orange"
+                    }
+                    if(i > 29 & i< 39){
+                        return "green"
+                    }
+                    if(i > 38 & i< 47){
+                        return "blue"
+                    }
+                    
                 })
 
-                .attr("x", function(d, i) {
-                    // return margin.left + i * (width / Dates.length);
-                    return margin.left
+                .attr("cx", function(d) {
+                    return xScale(d[1]);
                 })
-                .attr("y", function(d){
-                    return yScale(d)
+                .attr("cy", function(d){
+                    return  yScale(d[0]);
                 })
                 .attr("r", 5)
-                // .attr("width", width / Dates.length)
-                .attr("width", width)
-                .attr("height", function(d){
-                    return height - yScale(d) 
-                })
-            // try to make x-axis
+                
+            // add x-axis
             svg.append("g")
                 .attr("class", "axis") 
-                .attr("transform", "translate(0," + height + ")")
+                .attr("transform", "translate("+[ 0, height - margin.bottom - margin.top]+")")
                 .call(xAxis);
+            // label x-axis
             svg.append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("x", margin.left)
-                .style("text-anchor", "end")
-                .text("Women in science");
-            // try to make y-axis
+                .attr("transform", "rotate(0)")
+                .attr("y", height)
+                .attr("x", width / 2)
+                .style("text-anchor", "begin")
+                .text("Percentage of women in science");
+            // add y-axis
             svg.append("g")
                 .attr("class", "axis")
-                .attr("transform", "translate("+[margin.left, margin.bottom]+")")
+                .attr("transform", "translate(" + padding + ",0)")
                 .call(yAxis)
-            // try to name the y-axis, was still working on it
+            // label y-axis
             svg.append("text")
                 .attr("transform", "rotate(-90)")
-                .attr("y", margin.right)
+                .attr("x", 0 - padding)
+                .attr("y", margin.top)
                 .style("text-anchor", "end")
-                .text("Consumer Satisfaction");
+                .text("Consumer Satisfaction")
+
+            var legend = svg.selectAll(".legend")
+                .data(Colours1.keys)
+                .enter()
+                .append("g")
+                .classed("legend", true)
+                .attr("transform", function(d, i) {
+                    return "translate(0," + i * 20 + ")";
+                });
+            legend.append("rect")
+                .attr("x", width + 10)
+                .attr("width", 12)
+                .attr("height", 12)
+                .style("fill", "black");
     }).catch(function(e){
         throw(e);
     });
@@ -222,4 +263,4 @@ function transformResponse(data){
 
     // return the finished product!
     return dataArray;
-}
+};

84775290548266579d8f00c5ead8a058785c4266
misterwhybe <wiebejelsma@hotmail.com>
2018-12-11 20:53:29 +0100

    getting started

---
+++ Homework/Week_6/Change_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+# def load_csv(self):
+
+data = pd.read_csv("Dataset_students.csv")
+data = data[(data["Inequality"] == "Total")]
+data = data.drop(["LOCATION", "INDICATOR", "Reference Period", "Flag Codes"\
+,"MEASURE", "PowerCode", "Reference Period Code", "Flags", "Unit Code", \
+"PowerCode Code", "Measure", "Inequality", "INEQUALITY"], axis = 1)
+data = data[(data["Indicator"] == "Household net adjusted disposable income") | (data["Indicator"] =="Years in education")]
+data.to_json("data.json")
+
+# def use_json(self):
+#     d3.json("data.json").then(function(data){
+
---
+++ Homework/Week_6/linked_views.js
@@ -0,0 +1,5 @@
+// Wiebe Jelsma (12468223)
+window.onload = function() {
+    data = d3.json("data.json")
+    console.log(data);
+}
